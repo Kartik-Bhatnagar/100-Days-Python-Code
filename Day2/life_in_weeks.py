f3d17aeec753b306b30b76eda4c2f648048bf452
Kartik-Bhatnagar <80669502+Kartik-Bhatnagar@users.noreply.github.com>
2023-05-08 16:42:53 +0530

    Delete life_in_weeks.py

--- Day2/life_in_weeks.py
+++
@@ -1,6 +0,0 @@
-#Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
-
-age = input("What is your current age? ")
-
-age_left = 90-int(age)
-print(f"You have {age_left*365} days, {(age_left)*(365//7)} weeks, and {age_left*12} months left.")
