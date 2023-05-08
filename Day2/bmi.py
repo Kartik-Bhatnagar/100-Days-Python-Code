37684511e0d1334618cd08cfb78f3f83bdb0cea9
KartikBhatnagar <kartikbhatnagar111@gmail.com>
2023-05-08 17:01:04 +0530

    day2 restored

---
+++ Day2/bmi.py
@@ -0,0 +1,7 @@
+#Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+bmi = float(weight) / (float(height)*float(height))
+print(int(bmi))
