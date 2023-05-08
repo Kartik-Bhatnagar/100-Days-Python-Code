c568b00d4dfe6b4f82f504982991fa11f024cba9
Kartik-Bhatnagar <80669502+Kartik-Bhatnagar@users.noreply.github.com>
2023-05-08 16:42:29 +0530

    Delete bmi.py

--- Day2/bmi.py
+++
@@ -1,7 +0,0 @@
-#Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
-
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-
-bmi = float(weight) / (float(height)*float(height))
-print(int(bmi))
