8e54d52edcb3fbfd81b5d2a0e6fa70685a142899
KartikBhatnagar <kartikbhatnagar111@gmail.com>
2023-05-12 11:19:10 +0530

    add 3 files

---
+++ Day5/FizzBuzz_game.py
@@ -0,0 +1,19 @@
+# write a program that automatically prints the solution to the FizzBuzz game.
+
+"""Your program should print each number from 1 to 100 in turn.
+
+When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+
+When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+
+  And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"""
+for i in range(1,101):
+    msg =""
+    if i%3 == 0:
+        msg ="Fizz"
+    if i%5 ==0:
+        msg +="Buzz"    
+    if len(msg)>0:
+        print(msg)
+    else:
+        print(i)    
---
+++ Day5/even_num_sum.py
@@ -0,0 +1,5 @@
+#calculates the sum of all the even numbers from 1 to 100.
+sm =0
+for i in range(0,101,2):
+    sm += i
+print(sm)    
