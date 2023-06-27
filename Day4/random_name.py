8a18a689b29b97df2c138f008a67ed639d36c0fa
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2023-06-27 14:25:15 +0530

    check contribution

--- Day4/random_name.py
+++ Day4/random_name.py
@@ -4,4 +4,4 @@ import random
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(",")
-print(f"{names[random.randint(0,len(names)-1)]} is going to buy meal today!")
+print(f"{names[random.randint(0,len(names)-1)]} is going to buy meal today !")
